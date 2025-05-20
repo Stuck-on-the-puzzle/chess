d8679f6bc3ca7eb5cfc85927a3fe57e952166d27
Stuck-on-the-puzzle <nathanjstucki@gmail.com>
2025-05-19 18:41:16 -0600

    implemented ClearService

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,7 +1,21 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+
 public class ClearService {
 
     // implements the main functions of the program (one the seven functions)
     // this class implements the clear function
+
+    UserDAO userDAO;
+    AuthDAO authDAO;
+    GameDAO gameDAO;
+
+    public void clear() {
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+    }
 }
