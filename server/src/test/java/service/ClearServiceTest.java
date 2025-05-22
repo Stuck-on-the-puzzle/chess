dfcc38e18eaa7ffc7101fdb099859afa2983f61b
Stuck-on-the-puzzle <nathanjstucki@gmail.com>
2025-05-21 23:21:06 -0600

    implemented ClearService tests

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,33 +1,47 @@
-//package service;
-//
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import passoff.model.TestAuthResult;
-//import passoff.model.TestCreateRequest;
-//import passoff.model.TestUser;
-//import passoff.server.TestServerFacade;
-//import server.Server;
-//
-//public class ClearServiceTest {
-//
-//    @BeforeAll
-//    public static void init() {
-//        server = new Server();
-//        var port = server.run(0);
-//        System.out.println("Started test HTTP server on " + port);
-//
-//        serverFacade = new TestServerFacade("localhost", Integer.toString(port));
-//        existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
-//        newUser = new TestUser("NewUser", "newUserPassword", "nu@mail.com");
-//        createRequest = new TestCreateRequest("testGame");
-//    }
-//
-//    @BeforeEach
-//    public void setup() {
-//        serverFacade.clear();
-//
-//        //one user already logged in
-//        TestAuthResult regResult = serverFacade.register(existingUser);
-//        existingAuth = regResult.getAuthToken();
-//    }
-//}
+package service;
+
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import service.RequestResult.LoginRequest;
+import service.RequestResult.LoginResult;
+import service.RequestResult.RegisterResult;
+
+public class ClearServiceTest {
+
+    static UserDAO userDAO;
+    static AuthDAO authDAO;
+    static GameDAO gameDAO;
+    static UserService userService;
+    static ClearService clearService;
+
+    static UserData user;
+
+    @BeforeAll
+    public static void init() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+        userService = new UserService(userDAO, authDAO);
+        clearService = new ClearService(userDAO, authDAO, gameDAO);
+    }
+
+    @BeforeEach
+    public void setup() {
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+        user = new UserData("username", "password", "email@email.com");
+    }
+
+    @Test
+    @DisplayName("Clear Test Positive")
+    public void clearPos() throws DataAccessException {
+        RegisterResult result = userService.register(user);
+        clearService.clear();
+        // make sure there are not authTokens or users in database
+        Assertions.assertThrows(DataAccessException.class, () -> authDAO.getAuth(result.authToken()));
+        Assertions.assertThrows(DataAccessException.class, () -> userDAO.getUser(user.username()));
+    }
+}
