ca84f028803d2f2cee002108f9e857379332c33c
Stuck-on-the-puzzle <nathanjstucki@gmail.com>
2025-05-21 23:06:58 -0600

    implemented UserService tests made by me

---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,22 @@
+//package service;
+//
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import passoff.model.TestAuthResult;
+//
+//public class GameServiceTest {
+//
+//    @BeforeAll
+//    public static void init() {
+//
+//    }
+//
+//    @BeforeEach
+//    public void setup() {
+//        serverFacade.clear();
+//
+//        //one user already logged in
+//        TestAuthResult regResult = serverFacade.register(existingUser);
+//        existingAuth = regResult.getAuthToken();
+//    }
+//}
---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,84 @@
+package service;
+
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import service.RequestResult.LoginRequest;
+import service.RequestResult.LoginResult;
+import service.RequestResult.RegisterResult;
+
+public class UserServiceTest {
+
+    static UserDAO userDAO;
+    static AuthDAO authDAO;
+    static UserService userService;
+
+    static UserData user;
+
+    @BeforeAll
+    public static void init() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO, authDAO);
+    }
+
+    @BeforeEach
+    public void setup() {
+        userDAO.clear();
+        authDAO.clear();
+        user = new UserData("username", "password", "email@email.com");
+    }
+
+    @Test
+    @DisplayName("Register Test Positive")
+    public void registerUserPos() throws DataAccessException {
+        RegisterResult result = userService.register(user);
+        String authToken = result.authToken();
+        AuthData authData = new AuthData(authToken, user.username());
+        Assertions.assertEquals(authDAO.getAuth(authToken), authData);
+    }
+
+    @Test
+    @DisplayName("Register Test Negative")
+    public void registerUserNeg() {
+        UserData badUser = new UserData(null, "password", "email@email.com");
+        Assertions.assertThrows(DataAccessException.class, () -> userService.register(badUser));
+    }
+
+    @Test
+    @DisplayName("Login Test Positive")
+    public void loginUserPos() throws DataAccessException {
+        RegisterResult result = userService.register(user);
+        LoginRequest loginRequest = new LoginRequest(user.username(),user.password());
+        LoginResult loginResult = userService.login(loginRequest);
+        AuthData authData = new AuthData(loginResult.authToken(), user.username());
+        Assertions.assertEquals(authDAO.getAuth(loginResult.authToken()), authData);
+    }
+
+    @Test
+    @DisplayName("Login Test Negative")
+    public void loginUserNeg() throws DataAccessException {
+        userService.register(user);
+        LoginRequest loginRequest = new LoginRequest(user.username(),"notRightPassword");
+        Assertions.assertThrows(DataAccessException.class, () -> userService.login(loginRequest));
+    }
+
+    @Test
+    @DisplayName("Logout Test Positive")
+    public void logoutUserPos() throws DataAccessException {
+        userService.register(user);
+        LoginRequest loginRequest = new LoginRequest(user.username(),user.password());
+        LoginResult loginResult = userService.login(loginRequest);
+        userService.logout(loginResult.authToken());
+        // check that the authToken from login is no longer found
+        Assertions.assertThrows(DataAccessException.class, () -> authDAO.getAuth(loginResult.authToken()));
+    }
+
+    @Test
+    @DisplayName("Logout Test Negative")
+    public void logoutUserNeg() throws DataAccessException {
+        // check that unauthorized token causes an error
+        Assertions.assertThrows(DataAccessException.class, () -> userService.logout("randomAuth"));
+    }
+}
