e65dbc6b828a02ab5b9afc0d128ab1cfd3771fb1
Stuck-on-the-puzzle <nathanjstucki@gmail.com>
2025-06-03 22:19:33 -0600

    Updated Repl so that Client main runs, but still has a few bugs

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -10,31 +10,62 @@ public class Repl {
     private final PostLoginClient postLoginClient;
 //    private final GameplayClient gameplayClient;
     private String state = "Logged Out";
+    private String username;
+    private String authToken;
 
     public Repl(String serverUrl) {
         preLoginClient = new PreLoginClient(serverUrl);
         postLoginClient = new PostLoginClient(serverUrl);
 //        gameplayClient = new GameplayClient(serverUrl);
     }
+
     public void run() {
         String whiteKing = "♔";
         String blackKing = "♚";
-        System.out.println("♚ Welcome to Chess. Type help to get started.");
+        System.out.println("♚ Welcome to Chess. Type help to get started ♚");
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
         while (!result.equals("quit")) {
             printPrompt();
+
             String line = scanner.nextLine();
 
             try {
-                result = client.eval(line);
-                System.out.println(GREEN + result);
+                if (state.equals("Logged Out")) {
+                    result = preLoginClient.eval(line);
+
+                    if (result.startsWith("Logged in as")) {
+                        String[] parts = result.split(" ");
+                        username = parts[3].replace(".", "");
+                        state = "Logged in";
+                    }
+                }
+
+                else if (state.equals("Logged in")) {
+                    result = postLoginClient.eval(line);
+
+                    if (line.toLowerCase().startsWith("logout")) {
+                        state = "Logged Out";
+                        username = null;
+                        authToken = null;
+                    }
+                }
+
+                System.out.println(result);
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
             }
         }
         System.out.println();
+    }
+
+    private void printPrompt() {
+        if (state.equals("Logged Out")) {
+            System.out.print("[LOGGED OUT] >>> ");
+        } else if (state.equals("Logged in")) {
+            System.out.print("[LOGGED IN as " + username + "] >>> ");
+        }
     }
 }
